3a3df6265c29fd1f25714e8a7bbd9a0dcdd7605f
Evan-FRC <egwiz01@gmail.com>
2022-04-14 04:42:46 -0700

    changed event code

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022marea",
+        "defaultValue":"2022necmp1",
         "required":"true",
         "disabled":"true"
       },
